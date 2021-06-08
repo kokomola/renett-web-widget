fc64a9242a1c94d1a28247baece6055ca6bef7ed
Tim <kokomola2@gmail.com>
2021-06-08 23:03:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player'
 
-function App() {
+const host = 'https://renett.botlify.io';
+
+const App = props => {
+
+  const [videos, setVideos] = useState([]);
+  
+  useEffect(() => {
+    (async () => {
+      if (videos.length === 0) {
+        const getNettsRes = await getNetts('new', []);
+        const videoArr = [];
+        getNettsRes.data.map(nett => {
+          const splited = nett.video.split('.');
+          if (splited[splited.length - 1] === 'm3u8') {
+            videoArr.push(nett);
+          }
+        })
+        setVideos(videoArr);
+      }
+    })();
+  });
+
+  const getNetts = async (order, spheres, skip = 0) => {
+    const sphereQuery = spheres.map(sp => `&spheres[]=${sp.title}`).join('');
+    const headers = {'content-type': 'application/json'};
+    const response = await fetch(`${host}/netts?order=${order}${sphereQuery}&skip=${skip}`, {
+      method: 'get',
+      headers,
+    });
+    const json = await response.json();
+  
+    return {data: json.data, status: response.status};
+  };
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      paritialVisibilityGutter: 60
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      paritialVisibilityGutter: 50
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      paritialVisibilityGutter: 30
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Carousel
+      ssr
+      partialVisbile
+      deviceType={props.deviceType}
+      // itemClass="image-item"
+      responsive={responsive}
+    >
+      {videos.map(vid => {
+        return (
+          <ReactPlayer className='react-player'
+          // This is the video address passed from the superior page
+          url={vid.video}
+          playing
+          width='95%'
+          controls
+          
+          // config={{
+          //   file: {
+          //     forceHLS: true,
+          //   }
+          // }}
+  />
+          // <iframe src="https://videodelivery.net/b841a7bc853745478652feab89545b72/manifest/video.m3u8" style={{border: 'none', height: '500px', width: '98%'}} allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true"></iframe>
+        );
+      })}
+    </Carousel>
     </div>
   );
 }
